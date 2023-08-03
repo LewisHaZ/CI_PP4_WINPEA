f6681b6139dd42fed40a60bd89330f8423de8f51
LewisHaZ <feldman.lewis@Gmail.com>
2023-08-03 15:44:02 +0000

    uninstall currently installed packages due to an unresolvable error

--- winpea_world/settings.py
+++ winpea_world/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book_a_slot'
+    'book_a_slot',
 ]
 
 MIDDLEWARE = [
@@ -77,12 +77,15 @@ WSGI_APPLICATION = 'winpea_world.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
 }
 
 
