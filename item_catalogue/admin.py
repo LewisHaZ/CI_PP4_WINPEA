6204498b731e22be31cea4515f1e8140fec3178a
LewisHaZ <feldman.lewis@Gmail.com>
2023-08-03 18:29:33 +0000

    add list display, search fields and list filter to ProductItem app

--- item_catalogue/admin.py
+++ item_catalogue/admin.py
@@ -6,4 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(ProductItem)
 class ProductItemAdmin(SummernoteModelAdmin):
 
+    list_display = ('item_name', 'item_type', 'price', 'available')
+    search_fields = ('item_name', 'description')
+    list_filter = ('available', 'item_type')
     summernote_fields = ('description')
