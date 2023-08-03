fdb9414c16d13d81ce8cd5d34a6341d63a8df2aa
LewisHaZ <feldman.lewis@Gmail.com>
2023-08-03 16:36:42 +0000

    add x_frame_options method to settings.py file

--- winpea_world/settings.py
+++ winpea_world/settings.py
@@ -29,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['winpea-world.herokuapp.com', 'localhost', '8000-lewishaz-cipp4winpea-mzbticeyhxd.ws-eu102.gitpod.io']
 
