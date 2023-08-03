cef8bd76967a93f0fff0196d22f1b30ea72e93a6
LewisHaZ <feldman.lewis@Gmail.com>
2023-08-03 16:07:25 +0000

    add localhost to allowed hosts for heroku initial deployment

--- winpea_world/settings.py
+++ winpea_world/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-lewishaz-cipp4winpea-mzbticeyhxd.ws-eu102.gitpod.io', 'winpea_world.herokuapp.com']
+ALLOWED_HOSTS = ['8000-lewishaz-cipp4winpea-mzbticeyhxd.ws-eu102.gitpod.io', 'winpea_world.herokuapp.com', 'localhost']
 
 
 # Application definition
