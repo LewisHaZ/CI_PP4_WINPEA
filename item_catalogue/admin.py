48d025c9dd37194436a0a67b7481350a8c56c442
LewisHaZ <feldman.lewis@Gmail.com>
2023-08-03 18:21:24 +0000

    install summernote, import to admin.py and add class decorator

--- item_catalogue/admin.py
+++ item_catalogue/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import ProductItem
-# Register your models here.
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(ProductItem)
+
+@admin.register(ProductItem)
+class ProductItemAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
--- winpea_world/settings.py
+++ winpea_world/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'book_a_slot',
     'item_catalogue',
 ]
