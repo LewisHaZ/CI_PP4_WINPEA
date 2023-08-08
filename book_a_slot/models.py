b85e940ba2b808f4ef7d08e2fdfbd7d219487b5e
LewisHaZ <feldman.lewis@Gmail.com>
2023-08-08 16:57:31 +0000

    Add booking class model & begin user class

--- book_a_slot/models.py
+++ book_a_slot/models.py
@@ -16,3 +16,29 @@ class Slot(models.Slot):
 
     def __str__(self):
         return self.slot_name
+
+
+class Booking(models.Model):
+    """
+    a class for the booking model
+    """
+    booking_id = models.AutoField(primary_key=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    requested_date = models.DateField()
+    requested_time = models.CharField(
+        max_length=25, choices=time_slots, default='10:00')
+    slot = models.ForeignKey(
+        Slot, on_delete=models.CASCADE, related_name="slot_reserved",
+        null=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user', null=True)
+    status = models.CharField(max_length=25, unique=True)
+
+    class Meta:
+        ordering = ['-requested_time']
+  
+    def __str__(self):
+        return self.booking_id
+  
+
+class User(models.Model):
