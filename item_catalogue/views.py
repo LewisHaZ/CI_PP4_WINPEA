6e3e1dcdafba8773206b31576c8cc7366be98e6e
LewisHaZ <feldman.lewis@Gmail.com>
2023-08-08 18:09:20 +0000

    Add ProductList to views.py in Catalogue app

--- item_catalogue/views.py
+++ item_catalogue/views.py
@@ -1,3 +1,19 @@
+# Imports
+# 3rd Party
 from django.shortcuts import render
+from django.views import generic
+# Internal
+from .models import ProductItem
 
-# Create your views here.
+
+class ProductList(generic.ListView):
+    """
+    This is the view for all the products available
+    in the catalogue
+    """
+    model = ProductItem
+    queryset = ProductItem.objects.filter(available=1).order_by('-item_name')
+    template_name = 'catalogue.html'
+
+
+
