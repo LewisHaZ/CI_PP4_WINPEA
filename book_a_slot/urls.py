45d5cf590baa56b7940c2d4467f461b249ffbf06
LewisHaZ <feldman.lewis@Gmail.com>
2023-08-09 18:21:37 +0000

    Add templates folder in book a slot app & initialise views

---
+++ book_a_slot/urls.py
@@ -0,0 +1,6 @@
+# Imports
+# 3rd Party
+from django.urls import path
+
+# Internal
+from book_a_slot import views
--- book_a_slot/views.py
+++ book_a_slot/views.py
@@ -1,3 +1,7 @@
+# Imports
+# 3rd Party
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+# Internal
+from .models import Slot, Guest, Booking
