0f3d31afe0c70d35a4f9ce3a548c9f49e1ee2b6c
LewisHaZ <feldman.lewis@Gmail.com>
2023-08-11 07:15:08 +0000

    Remove unused Guest code in models.py

--- book_a_slot/models.py
+++ book_a_slot/models.py
@@ -45,23 +45,6 @@ class Slot(models.Model):
         return self.slot_name
 
 
-# class Guest(models.Model):
-#     """
-#     a class for the User model
-#     """
-#     guest_id = models.AutoField(primary_key=True)
-#     created_date = models.DateTimeField(auto_now_add=True)
-#     name = models.CharField(max_length=80)
-#     email = models.EmailField(max_length=254, default="")
-#     phone = PhoneNumberField()
-
-    # class Meta:
-    #     ordering = ['-created_date']
-
-    # def __str__(self):
-    #     return self.name
-
-
 class Booking(models.Model):
     """
     a class for the booking model
@@ -76,7 +59,7 @@ class Booking(models.Model):
         null=True)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="user", null=True)
-    name = models.CharField(max_length=50, null=True, blank=True)
+    name = models.CharField(max_length=50, null=True)
     email = models.EmailField(max_length=200, default="")
     phone = PhoneNumberField(null=True)
     status = models.CharField(
