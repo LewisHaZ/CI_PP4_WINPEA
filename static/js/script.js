62a3d070d7b98e9f9edc0e36bb330aa41bc40a95
LewisHaZ <feldman.lewis@Gmail.com>
2023-08-23 16:23:08 +0000

    Add files for test cases in book a slot app and item catalogue app

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,7 @@
-setTimeout(function () {
-    let messages = document.getElementById('msg');
-    let alert = new bootstrap.Alert(messages);
-    alert.close();
-}, 2500);
+$(document).ready(function(){
+    setTimeout(function () {
+        let messages = document.getElementById('msg');
+        let alert = new bootstrap.Alert(messages);
+        alert.close();
+    }, 2500);
+});
