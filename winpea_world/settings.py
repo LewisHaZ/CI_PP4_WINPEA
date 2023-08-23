9988c28fd03136a2cad81ce056513d4a88fa76da
LewisHaZ <feldman.lewis@Gmail.com>
2023-08-23 16:26:08 +0000

    Adjust settings.py for testing purposes with seperate databases

--- winpea_world/settings.py
+++ winpea_world/settings.py
@@ -116,10 +116,19 @@ WSGI_APPLICATION = 'winpea_world.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
-
+if os.environ.get("DEVELOPMENT") == "True":
+    # Testing Database
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    # Heroku Database
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
